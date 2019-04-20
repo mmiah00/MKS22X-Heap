1a828029c9349f2674e50a14708c01facef32dcc
mmiah <mmiah00@stuy.edu>
2019-04-20 16:15:37 -0400

    working on pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,9 +1,32 @@
 public class MyHeap {
   //We discussed these 2 methods already:
-  private static void pushDown(int[]data,int size,int index){}
+  private static void pushDown(int[]data,int size,int index){
+    while (index * 2 + 2 < size) {
+      int left, right;
+      left = data[index * 2 + 1];
+      right = data[index * 2 + 2];
+
+      if (data[index] > left){
+        int temp = data[index];
+        data[index] = left;
+        data[index * 2 + 1] = temp;
+        index = index * 2 + 1;
+      }
+      else {
+        int temp = data[index];
+        data[index] = right;
+        data[index * 2 + 2] = temp;
+        index = index * 2 + 2;
+      }
+    }
+
+  }
   /*
        - size  is the number of elements in the data array.
-       - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+       - push the element at index i downward into the correct position.
+         This will swap with the larger of the child nodes provided that
+         child is larger. This stops when a leaf is reached, or neither
+         child is larger. [ should be O(logn) ]
        - precondition: index is between 0 and size-1 inclusive
        - precondition: size is between 0 and data.length-1 inclusive.
   */
