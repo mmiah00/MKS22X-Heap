e8038631e34be4678443fb8093bb3359e186dd28
mmiah <mmiah00@stuy.edu>
2019-04-20 22:23:22 -0400

    pushdown doesnt work

--- MyHeap.java
+++ MyHeap.java
@@ -1,25 +1,33 @@
 public class MyHeap {
   //We discussed these 2 methods already:
   private static void pushDown(int[]data,int size,int index){
+    /*
     while (index * 2 + 2 < size) {
       int left, right;
       left = data[index * 2 + 1];
       right = data[index * 2 + 2];
 
-      if (data[index] < left && left >= right){
-        int temp = data[index];
-        data[index] = left;
-        data[index * 2 + 1] = temp;
-        index = index * 2 + 1;
+      if (data[index] < left && data[index] < right) {
+        if (data[index] < left && left >= right){
+          int temp = data[index];
+          data[index] = left;
+          data[index * 2 + 1] = temp;
+          index = index * 2 + 1;
+        }
+        else {
+          int temp = data[index];
+          data[index] = right;
+          data[index * 2 + 2] = temp;
+          index = index * 2 + 2;
+        }
       }
       else {
-        int temp = data[index];
-        data[index] = right;
-        data[index * 2 + 2] = temp;
-        index = index * 2 + 2;
+        index = size;
       }
     }
-
+    //print (data);
+    */
+    
   }
 
   private static void pushUp(int[]data,int index){
@@ -32,19 +40,27 @@ public class MyHeap {
         index = (index - 1)/2;
       }
       else {
-        index = -1; 
+        index = -1;
       }
     }
   }
-  /*
-       - push the element at index i up into the correct position.
-         This will swap it with the parent node until the parent node
-         is larger or the root is reached. [ should be O(logn) ]
-       - precondition: index is between 0 and data.length-1 inclusive.
-  */
 
   //We will discuss this today:
-  public static void heapify(int[] data){}
+  public static void heapify(int[] data){
+    for (int i = 0; i < data.length; i ++){
+      pushDown (data, data.length, i);
+    }
+
+    for (int i = data.length; i >= 0; i --) {
+      pushUp (data,i);
+    }
+  }
+
+  private static void print (int[] data) {
+    for (int num : data) {
+      System.out.print (num + " ");
+    }
+  }
   /*
       - convert the array into a valid heap. [ should be O(n) ]
   */
@@ -53,4 +69,11 @@ public class MyHeap {
   /*
       - sort the array by converting it into a heap then removing the largest value n-1 times. [ should be O(nlogn) ]
   */
+
+  public static void main (String[] args) {
+    int[] a = {15,87,24,12,42,18,19,37,48,98,26,1};
+    int[] b = {19,100,136,17,20,25,1,2,7,3,200};
+    heapify (a);
+    print (a);
+  }
 }
